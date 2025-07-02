afb820735a3d1315ce3715af86e51650c385314a
Clef <hopecwedu@gmail.com>
2025-07-02 17:08:07 +0900

    First

---
+++ projectbob/src/main/java/com/projectbob/ProjectbobApplication.java
@@ -0,0 +1,13 @@
+package com.projectbob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectbobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectbobApplication.class, args);
+	}
+
+}
---
+++ projectbob/src/test/java/com/projectbob/ProjectbobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectbob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectbobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
