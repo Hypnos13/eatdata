6683513e3de689b1be77aa0c92cfac078ee77ec2
dydy6230 <dydy6230@naver.com>
2025-07-08 17:51:12 +0900

    fix 깃 충돌 정리

--- projectbob/src/main/java/com/projectbob/controller/LoginController.java
+++ projectbob/src/main/java/com/projectbob/controller/LoginController.java
@@ -6,12 +6,10 @@ import java.io.PrintWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.projectbob.ProjectbobApplication;
 import com.projectbob.domain.Member;
 import com.projectbob.service.LoginService;
 
