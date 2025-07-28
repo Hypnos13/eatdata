37de9c5482eb38b37d3d37d30aa0898b40f3ad15
Clef <hopecwedu@gmail.com>
2025-07-28 12:09:41 +0900

    fix:오류수정

--- projectbob/src/main/java/com/projectbob/configurations/WebConfig.java
+++ projectbob/src/main/java/com/projectbob/configurations/WebConfig.java
@@ -1,10 +1,6 @@
 package com.projectbob.configurations;
 
-<<<<<<< HEAD
-
 import org.springframework.beans.factory.annotation.Value;
-=======
->>>>>>> Yong
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
