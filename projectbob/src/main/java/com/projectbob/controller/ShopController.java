35c35f4104d72c334df674d2126a6b572e53d635
Forest <jhrim1995@gmail.com>
2025-08-05 11:18:18 +0900

    수정중

--- projectbob/src/main/java/com/projectbob/controller/ShopController.java
+++ projectbob/src/main/java/com/projectbob/controller/ShopController.java
@@ -139,6 +139,7 @@ public class ShopController {
 		return "redirect:shopMain";
 	}
 	
+	//배달 대행 요청
 	@GetMapping("/shop/delivery")
 	public String deliveryDispatchPage(
 	        @SessionAttribute(name = "loginId", required = false) String loginId,
@@ -176,6 +177,11 @@ public class ShopController {
 	    model.addAttribute("deliveryAgencies", deliveryAgencies);
 
 	    return "shop/delivery"; 
+	}
+	//라이더 페이지
+	@GetMapping("/rider/request")
+	public String riderRequestPage() {
+	    return "rider/rider_request";
 	}
 
 	/* ----------------------- 메인 ----------------------- */
